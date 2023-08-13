6501bb35301872f578160697eb9f4ee452df37f1
htiwari29 <htiwari292001@gmail.com>
2023-08-13 16:01:07 +0530

    Initial structure

---
+++ src/test/java/com/bookit/bifrost/BifrostApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bookit.bifrost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class BifrostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
